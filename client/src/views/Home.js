38ad82aacd6ca75f24baedd5404640ae515fdadf
Bao Xiong <bao.xiong2011@gmail.com>
2021-05-23 14:07:46 -0500

    Added timer and grid onto the homepage.

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,14 +1,17 @@
 import React, { Fragment } from "react";
 
-import Hero from "../components/Hero";
-import Content from "../components/Content";
+import { Row, Container } from "../components/Grid";
+import TimerApp from "../components/Timer/App";
+//timer app and grid component is not included in here yet.
 
 const Home = () => (
-  <Fragment>
-    <Hero />
-    <hr />
-    <Content />
-  </Fragment>
+  <Container fluid>
+    <Row>
+      <p>
+      <TimerApp />
+      </p>
+    </Row>
+  </Container>
 );
 
 export default Home;
