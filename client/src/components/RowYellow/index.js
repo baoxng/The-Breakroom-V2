db773d11506dcfaef54d1367d8322b16139befed
mesahoang <me.sa.hoang@gmail.com>
2021-05-24 21:01:24 -0500

    added spacedash link to hp

--- client/src/components/RowYellow/index.js
+++ client/src/components/RowYellow/index.js
@@ -8,10 +8,14 @@ const YellowRow = () => {
       <Container className="yellow-container" fluid={true}>
       <Row>
         <Col xs="4">
-          <h1>Narrow Escape</h1>
+          <h1>Space Dash</h1>
           <p>Let's Play >></p>
         </Col>
-        <Col xs="8">image here</Col>
+        <Col xs="8">
+          <Link to="/spacedash">
+            <Media src="https://i.ibb.co/ph40G5F/space-dash.png" alt="Space Dash"/>
+          </Link>
+        </Col>
       </Row>
       </Container>
     </>
