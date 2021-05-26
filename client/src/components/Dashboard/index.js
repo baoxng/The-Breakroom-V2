c23f9f13e63212b1aea17713fdb3aab2ba7c5259
mesahoang <me.sa.hoang@gmail.com>
2021-05-25 20:53:12 -0500

    created game dashboard component

--- client/src/components/Dashboard/index.js
+++ client/src/components/Dashboard/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Container, Col, Row } from 'reactstrap';
-import GameAmoeba from '../GameAmoeba';
+import { Container, Col, Row, Media } from 'reactstrap';
+import { Link } from "react-router-dom";
 import './App.css';
 
 const DashboardGames = () => {
@@ -9,28 +9,32 @@ const DashboardGames = () => {
       <Container className="dark-container" fluid={true}>
       <Row>
         <Col xs="12">
-        <h3>Amoeba Online</h3>
-        <p>Play as an amoeba in this single-cell battle! Compete against 100 bots in this fun, easy-to-learn game.</p>
         <hr></hr>
-        </Col>
-        <Col xs="3">
-        <div className="gameDesc">
-            <div className="gameDescBorder">
-            <h5>How to Play:</h5>
-            <p>Eating cells will help you grow larger. The larger you are, the bigger cells you can eat, which will allow you to monch the competition. Just don't get eaten in your quest to be the biggest!</p>
-            <h5>Controls:</h5>
-            <p>Mobile: Your amoeba will go to where the screen is touched.</p>
-            <p>PC: Your amoeba will follow your cursor.</p>
-            </div>
-        </div>
-        </Col>
-        <Col xs="9">
-          <GameAmoeba />
-        </Col>
-        <Col xs="12">
-        <hr></hr>
-        <h5>Credit:</h5>
-        <p>Taken from <a href="https://play.idevgames.co.uk/game/amoeba-online">this site</a> and created by <a href="https://play.idevgames.co.uk/profile/iDev">iDev</a>.</p>
+        <h1>Space Dash</h1>
+        <Link to="/spacedash">
+              <Media src="https://i.ibb.co/ph40G5F/space-dash.png" alt="Space Dash"/>
+        </Link>
+        <br></br>
+        <h1>Fruit Machine</h1>
+        <Link to="/fruitmachine">
+              <Media src="https://i.ibb.co/gVZHGtX/fruit-machine.png" alt="Fruit Machine"/>
+        </Link>
+        <br></br>
+        <h1>Amoeba Online</h1>
+        <Link to="/amoebaonline">
+              <Media src="https://i.ibb.co/Km7qc0R/amoeba-online.jpg" alt="Amoeba Online"/>
+        </Link>
+        <br></br>
+        <h1>Narrow Escape</h1>
+        <Link to="/narrowescape">
+              <Media src="https://i.ibb.co/cLFck5H/narrow-escape.png" alt="Narrow Escape"/>
+        </Link>
+        <br></br>
+        <h1>Tap Slime</h1>
+        <Link to="/tapslime">
+              <Media src="https://i.ibb.co/3fzfTHR/tap-slime.png" alt="Tap Slime"/>
+        </Link>
+        <br></br>
         </Col>
       </Row>
       </Container>
