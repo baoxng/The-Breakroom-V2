be6dda3c4f943749b0dd8bec54c7d4dc16e98e7f
mesahoang <me.sa.hoang@gmail.com>
2021-05-25 21:44:35 -0500

    updated hp description

--- client/src/components/DescriptionRow/index.js
+++ client/src/components/DescriptionRow/index.js
@@ -8,9 +8,8 @@ const DescriptionCon = () => {
       <Row>
         <Col xs="12">
         <h3>Welcome to The Breakroom</h3>
-        <p>The Breakroom is a mobile game app that not only encourages you to take a 15-minute break from work, but it holds you accountible to ONLY take 15 minutes off. Choose from our selection of games and have a great break!
+        <p>The Breakroom is a mobile-friendly game app that not only encourages you to take a 15-minute break from work, but it holds you accountable to only taking 15 minutes off. Our timer automatically ends your session after 15 minutes so you can get back to work. Choose from our selection of games and have a great break!
         </p>
-        <hr></hr>
         </Col>
       </Row>  
       </Container>
