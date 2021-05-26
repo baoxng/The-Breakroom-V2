93e1a5a26f7612dfa83f0ade3c1dad3f25cbe2e7
mesahoang <me.sa.hoang@gmail.com>
2021-05-25 21:40:43 -0500

    added description to hp

--- client/src/components/DescriptionRow/index.js
+++ client/src/components/DescriptionRow/index.js
@@ -8,7 +8,8 @@ const DescriptionCon = () => {
       <Row>
         <Col xs="12">
         <h3>Welcome to The Breakroom</h3>
-        <p>This is </p>
+        <p>The Breakroom is a mobile game app that not only encourages you to take a 15-minute break from work, but it holds you accountible to ONLY take 15 minutes off. Choose from our selection of games and have a great break!
+        </p>
         <hr></hr>
         </Col>
       </Row>  
