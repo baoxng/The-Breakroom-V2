defc4ce25a859b6a4770821ba6ed8007365e3c3f
Bao Xiong <bao.xiong2011@gmail.com>
2021-05-27 16:51:53 -0500

    Testing1.

---
+++ client/build/precache-manifest.7e765f40d94d1fb79ff6d665a650c566.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9e2897f778ca59e5b43829b503b01ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ee517dce61c92a99e0a",
+    "url": "/static/css/2.52f6e062.chunk.css"
+  },
+  {
+    "revision": "69b3cf486c9a2e82d8b2",
+    "url": "/static/css/main.823ba200.chunk.css"
+  },
+  {
+    "revision": "4ee517dce61c92a99e0a",
+    "url": "/static/js/2.d9ef80ea.chunk.js"
+  },
+  {
+    "revision": "03530a70479b271840cc246e50ce54ab",
+    "url": "/static/js/2.d9ef80ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fe7883fcfd737d01810",
+    "url": "/static/js/3.6993fa8e.chunk.js"
+  },
+  {
+    "revision": "69b3cf486c9a2e82d8b2",
+    "url": "/static/js/main.026c298c.chunk.js"
+  },
+  {
+    "revision": "abf1f4e7b7ed58b75b0c",
+    "url": "/static/js/runtime-main.bb1fb318.js"
+  },
+  {
+    "revision": "1499f61662a47060fa16058d04bd1ce7",
+    "url": "/static/media/EmojiOneColor.1499f616.woff2"
+  },
+  {
+    "revision": "0daa6ab9bbf363b6f8064fdf1763045f",
+    "url": "/static/media/NotoColorEmoji-flags.0daa6ab9.woff2"
+  },
+  {
+    "revision": "c5590569aab553af03722baa7d379978",
+    "url": "/static/media/loading.c5590569.svg"
+  },
+  {
+    "revision": "b3aba88d780e9eed234736fac30764f6",
+    "url": "/static/media/str-chat__connection-error.b3aba88d.svg"
+  },
+  {
+    "revision": "c65c7c39ad02d06012d7865e3943b19f",
+    "url": "/static/media/str-chat__icon-chevron-down.c65c7c39.svg"
+  }
+]);
