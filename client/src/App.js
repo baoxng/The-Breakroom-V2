31abf925da8369594936b30f2c662467813b5eb5
Bao Xiong <bao.xiong2011@gmail.com>
2021-05-26 21:45:56 -0500

    testing 3.

--- client/src/App.js
+++ client/src/App.js
@@ -43,6 +43,7 @@ const App = () => {
     <Router history={history}>
       <div id="app" className="d-flex flex-column h-100">
         <NavBar />
+        <p>Hi I was here!</p>
         <TimerApp />
         <Container className="flex-grow-1 mt-5">
           <Switch>
