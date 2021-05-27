a921587b74e9285bb598b0f69ef415fb86e0836d
Bao Xiong <bao.xiong2011@gmail.com>
2021-05-26 21:25:43 -0500

    Testing build.

--- client/build/precache-manifest.dbff778d4ea150adb524fefba876c69a.js
+++ client/build/precache-manifest.dbff778d4ea150adb524fefba876c69a.js
@@ -1,19 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
+    "revision": "d7f9df6dd064aa15fe7257b7d4eababa",
     "url": "/index.html"
   },
   {
+    "revision": "1bbbdb95776a965936cb",
     "url": "/static/css/2.11b452ed.chunk.css"
   },
   {
+    "revision": "f688ece70efeb5572fe5",
     "url": "/static/css/main.ce102372.chunk.css"
   },
   {
+    "revision": "1bbbdb95776a965936cb",
+    "url": "/static/js/2.d3ad4264.chunk.js"
   },
   {
     "revision": "24cba0c88f46204e4e9505858b825c5d",
+    "url": "/static/js/2.d3ad4264.chunk.js.LICENSE.txt"
   },
   {
+    "revision": "f688ece70efeb5572fe5",
+    "url": "/static/js/main.c2653b6c.chunk.js"
   },
   {
     "revision": "0231f95ac667b73c9187",
